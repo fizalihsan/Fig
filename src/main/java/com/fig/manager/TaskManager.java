583c9c2e99b58d92d1e9f22bc125c0e5011caf23
Fizal Mohamed <fizalihsan@gmail.com>
2013-11-24 12:39:13 -0500

    Adding business logic methods

--- src/main/java/com/fig/manager/TaskManager.java
+++ src/main/java/com/fig/manager/TaskManager.java
@@ -4,14 +4,19 @@ import com.fig.annotations.Transactional;
 import com.fig.domain.Task;
 import com.fig.util.Neo4jUtil;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Sets;
+import com.gs.collections.api.tuple.Pair;
 import org.neo4j.graphdb.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
+import java.util.Set;
 
 /**
- * Comment here about the class
+ * Helper class to bridge the gap between business logic and Neo4j.
+ *
+ * Methods annotated with @Transactional creates its own transaction automatically.
  * User: Fizal
  * Date: 11/22/13
  * Time: 5:28 PM
@@ -26,7 +31,7 @@ public class TaskManager {
      * <li> 2. For each node, the task properties are associated. </li>
      * <li> 3. Task dependencies are created. </li>
      * The entire processing is wrapped in a transaction.
-     * @param taskCollection
+     * @param tasks
      */
     @Transactional
     public void createTasks(Collection<Task> tasks){
@@ -48,9 +53,135 @@ public class TaskManager {
                 util.createTaskDependencies(task);
             }
             tx.success();
-            LOG.info("Users created successfully...");
+            LOG.info("{} Tasks created successfully...", tasks.size());
         } catch(Exception e) {
             throw new RuntimeException("Error creating task: " + currentTask, e);
+        } finally {
+            tx.finish();
+        }
+    }
+
+    /**
+     * Queries the graph database for the given task names. If none are found, an empty collection is returned.
+     * @param taskNames
+     * @return
+     */
+    public Set<Task> getTasks(Set<String> taskNames){
+        Set<Task> tasks = Sets.newHashSet();
+        for (String taskName : taskNames) {
+            final Task task = getNeo4jUtil().getTask(taskName);
+            if(task!=null){
+                tasks.add(task);
+            }
+        }
+
+        return tasks;
+    }
+
+    /**
+     * Properties in the given tasks are updated. Dependencies will not be updated.
+     * @param tasks
+     */
+    @Transactional
+    public void updateTaskProperties(Collection<Task> tasks){
+        LOG.info("Updating {} tasks...", tasks.size());
+        final Neo4jUtil util = getNeo4jUtil();
+        final Transaction tx = util.beginTransaction();
+        Task currentTask = null;
+        try {
+            for (Task task: tasks) {
+                currentTask = task;
+                util.updateTaskProperties(task);
+            }
+
+            tx.success();
+            LOG.info("{} Tasks updated successfully...", tasks.size());
+        } catch(Exception e) {
+            throw new RuntimeException("Error updating task: " + currentTask, e);
+        } finally {
+            tx.finish();
+        }
+    }
+
+    /**
+     * Deletes the given set of tasks. Throws exception if at least one of them couldn't be deleted.
+     * @param taskNames
+     */
+    @Transactional
+    public void deleteTasks(Set<String> taskNames){
+        final Neo4jUtil util = getNeo4jUtil();
+        final Transaction tx = util.beginTransaction();
+        String currentTask = "";
+        try {
+            for (String taskName : taskNames) {
+                currentTask = taskName;
+                util.deleteTask(taskName);
+            }
+            tx.success();
+            LOG.info("{} Tasks deleted successfully...", taskNames.size());
+        } catch (Exception e) {
+            throw new RuntimeException("Error deleting task: " + currentTask, e);
+        } finally {
+            tx.finish();
+        }
+    }
+
+    /**
+     * Creates dependencies between the given pair of tasks
+     * @param taskNamePairs
+     */
+    @Transactional
+    public void createTaskDependencies(Set<Pair<String, String>> taskNamePairs){
+        final Neo4jUtil util = getNeo4jUtil();
+        final Transaction tx = util.beginTransaction();
+        String currentFromTask = "", currentToTask = "";
+
+        try {
+            for (Pair<String, String> taskNamePair : taskNamePairs) {
+                final String fromTask = taskNamePair.getOne();
+                final String toTask = taskNamePair.getTwo();
+                currentFromTask = fromTask;
+                currentToTask = toTask;
+
+                util.createTaskDependency(fromTask, toTask);
+            }
+
+            tx.success();
+            LOG.info("{} task dependencies created successfully...", taskNamePairs.size());
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating dependency between tasks: " + currentFromTask + " -> " + currentToTask, e);
+        } finally {
+            tx.finish();
+        }
+    }
+
+    /**
+     * Deletes dependencies between the given pair of tasks
+     * @param taskNamePairs
+     */
+    @Transactional
+    public void deleteDependencies(Set<Pair<String, String>> taskNamePairs){
+        final Neo4jUtil util = getNeo4jUtil();
+        final Transaction tx = util.beginTransaction();
+        String currentFromTask = "", currentToTask = "";
+
+        try {
+            for (Pair<String, String> taskNamePair : taskNamePairs) {
+                final String fromTask = taskNamePair.getOne();
+                final String toTask = taskNamePair.getTwo();
+                currentFromTask = fromTask;
+                currentToTask = toTask;
+
+                final boolean dependencyDeleted = util.deleteTaskDependency(fromTask, toTask);
+                if(!dependencyDeleted){
+                    LOG.info("Dependency from {} -> {} either not found or not deleted !!! ", fromTask, toTask);
+                }
+            }
+
+            tx.success();
+            LOG.info("Task dependency deletion completed ...");
+        } catch (Exception e) {
+            throw new RuntimeException("Error deleting dependency between tasks: " + currentFromTask + " -> " + currentToTask, e);
         } finally {
             tx.finish();
         }
