40a8f9f5673ce3dacef3f5e7d4b19f9dd567be39
Fizal <fizalihsan@gmail.com>
2013-12-12 21:25:08 -0500

    Adding javadoc

--- src/test/groovy/com/fig/DesignPrincipleEnforcerSpec.groovy
+++ src/test/groovy/com/fig/DesignPrincipleEnforcerSpec.groovy
@@ -1,5 +1,6 @@
 package com.fig
 
+import groovy.io.FileType
 import org.apache.commons.io.FilenameUtils
 import spock.lang.Specification
 /**
@@ -12,6 +13,10 @@ class DesignPrincipleEnforcerSpec extends Specification {
 
     def "Leakage of Concern Check"() {
         def packageVsClasses = [
+                "org.apache.activemq" : [
+                        "MessagingUtil.java",
+                        "ServiceRequestConsumer.java"
+                ],
                 "com.google.gson" : [
                         "BindingUtil.java",
                         "SuccessResponse.java"
@@ -30,10 +35,14 @@ class DesignPrincipleEnforcerSpec extends Specification {
                         "ValidationResponse.java" //TODO avoid having JAX references in this class
                 ]
         ]
+
+        //TODO Java source path is hardcoded since there is no way to scan through the source files without providing
+        //absolute path. Reflection can't be used since the 'import' details are lost in bytecode. Source files could
+        //be included in the jar file but jarring happens only after the unit test completes successfully.
         def srcDir = new File('C:\\Fizal\\WorkArea\\SourceCode\\GitHubHome\\Fig\\src\\main\\java')
         def leakages = []
 
-        srcDir.eachFileRecurse { file ->
+        srcDir.eachFileRecurse(FileType.FILES) { file ->
             if (file.name =~ /.*\.java/) {
                 file.readLines().eachWithIndex { String line, int lineIndex ->
 
